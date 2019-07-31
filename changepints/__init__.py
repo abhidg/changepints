47ddb69f6aa41194a269f8b9a5654b282191ce2d
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2019-07-31 13:51:47 +0100

    add code for displaying changepoints

--- changepints/__init__.py
+++ changepints/__init__.py
@@ -1,2 +1,29 @@
+import numpy as np
+import ruptures as rpt
+import matplotlib.pyplot as plt
+import fire
+from .data import ChangePintsData
+
+class ChangePints:
+
+    def __init__(self, model="rbf", penalty=2):
+        self.model = model
+        self.penalty = penalty
+
+    def load(self, source):
+        self.data = ChangePintsData(source).data
+        self.signal = np.array(self.data.score)
+        algo = rpt.Pelt(model=self.model).fit(self.signal)
+        self.bkpts = algo.predict(pen=self.penalty)
+        return self
+
+    def breakpoints(self):
+        return self.bkpts
+
+    def show(self):
+        rpt.display(self.signal, self.breakpoints())
+        plt.show()
+
+
 def main():
-    return "Hello from ChangePINTS"
+    fire.Fire(ChangePints)
