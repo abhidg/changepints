514c5b13fb3e71a0694806761db505249bbc642b
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2019-08-01 13:37:29 +0100

    rename methods

--- changepints/__init__.py
+++ changepints/__init__.py
@@ -2,23 +2,25 @@ import numpy as np
 import ruptures as rpt
 import matplotlib.pyplot as plt
 import fire
-from .data import ChangePintsData
 
 class ChangePints:
 
-    def __init__(self, model="rbf", penalty=2):
+    def __init__(self, model="rbf", penalty=3):
         self.model = model
         self.penalty = penalty
 
-    def load(self, source):
-        self.data = ChangePintsData(source).data
-        self.signal = np.array(self.data.score)
+    def data(self, source):
+        self.signal = np.array(source)
         algo = rpt.Pelt(model=self.model).fit(self.signal)
         self.bkpts = algo.predict(pen=self.penalty)
         return self
 
     def breakpoints(self):
         return self.bkpts
+
+    def crossed_threshold(self, nbkpts=1):
+        "If number of breakpoints > nbkpts, return False"
+        return len(self.breakpoints()) > nbkpts
 
     def show(self):
         rpt.display(self.signal, self.breakpoints())
